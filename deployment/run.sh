7086dacb4adb597c0147d387bd87c6dafb3a3ef9
Vadym Kinchur <vadym.kinchur@gmail.com>
2023-12-31 15:33:15 +0200

    Set up elasticsearch and kibana, add prefix to all docker-compose services

--- deployment/run.sh
+++ deployment/run.sh
@@ -1,2 +1,4 @@
+#!/bin/sh
+
 docker compose -f docker-compose-kafka.yml -f docker-compose-spark.yml -f docker-compose-elastic.yml down
 docker compose -f docker-compose-kafka.yml -f docker-compose-spark.yml -f docker-compose-elastic.yml up
